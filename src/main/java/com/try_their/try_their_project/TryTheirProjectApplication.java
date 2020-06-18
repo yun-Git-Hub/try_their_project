35b159a7e624701e99d0bf90561620b521561ab3
yun-Git-Hub <17603840693@163.com>
2020-06-18 18:45:30 +0800

    initial commit

---
+++ src/main/java/com/try_their/try_their_project/TryTheirProjectApplication.java
@@ -0,0 +1,13 @@
+package com.try_their.try_their_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryTheirProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryTheirProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/try_their/try_their_project/TryTheirProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.try_their.try_their_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryTheirProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
